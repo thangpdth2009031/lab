38ff98c91ef742439705878c5e8cd7a1b512c008
Your Name <thangpdth2009031@fpt.edu.vn>
2021-08-25 09:26:23 +0700

    assigment

---
+++ resources/views/form.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <form action="">
+        <div class="form-group">
+            <label>Product code:</label>
+            <input type="text" class="form-control" id="email" placeholder="Enter product code" name="product-code">
+        </div>
+        <div class="form-group">
+            <label>Name:</label>
+            <input type="text" class="form-control" id="email" placeholder="Enter product code" name="name">
+        </div>
+        <div class="form-group">
+            <label>Price:</label>
+            <input type="text" class="form-control" id="email" placeholder="Enter product code" name="price">
+        </div>
+        <div class="form-group">
+            <label>Avatar:</label>
+            <input type="text" class="form-control" id="email" placeholder="Enter product code" name="avatar">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+</body>
+</html>
---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/',[\App\Http\Controllers\FurnitureController::class,'index']);
+Route::post('/store',[\App\Http\Controllers\FurnitureController::class,'store']);
